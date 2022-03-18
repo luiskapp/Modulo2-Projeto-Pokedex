c64391f1d0dc4d977b591774d19bce45a2f3839d
luiskapp <80328112+luiskapp@users.noreply.github.com>
2022-03-18 19:56:13 -0300

    Projeto

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+    res.render("index");
 });
-
-app.get("/index", (req, res) => {
-  res.render("index");
+app.get("/cadastro", (req, res) => {
+    res.render("index");
+});
+app.get("/detalhes", (req, res) => {
+    res.render("index");
 });
 
 app.listen(port, () =>
